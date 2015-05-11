cd9d38af628215c57a240cb71fc7dcdcffd9c8ed
Josh L <funinvegas@gmail.com>
2015-05-11 08:56:15 -0700

    Better starting world, and bettter user control

---
+++ Assets/PlayerBall/LazyCameraFollow.js
@@ -0,0 +1,218 @@
+﻿#pragma strict
+
+function Start () {
+	runnerNode = transform.FindChild("RunnerNode");
+}
+
+public var cameraObject:Camera;
+public var cameraNode:Transform;
+public var cameraLook:Transform;
+public var cameraRaycast:Transform;
+public var mouseLook:boolean = false;
+
+private var minDistance:double = 5;
+private var maxDistance:double = minDistance * 1.25;
+
+private var runnerNode:Transform;
+function averageVectorValue(v:Vector3) {
+	return (v.x + v.y + v.z) / 3;
+}
+
+function orientRunnerAndLook() {
+	// Rotate the runner so he is always looking away from the camera
+	runnerNode.transform.eulerAngles = new Vector3(0,Camera.main.transform.eulerAngles.y,0);
+
+	// Get the player size, accounting for scale of the player node
+	var playerSize:double = GetComponent(SphereCollider).radius * averageVectorValue(transform.localScale);
+
+	// Move the node the camera looks at to be above the player ball node
+	cameraLook.position = transform.position;
+	cameraLook.position.y += playerSize * 1;
+}
+
+function rotateViewByKey() {
+	var panLeft = Input.GetKeyDown ('q');
+	var panRight = Input.GetKeyDown ('e');
+
+	if (panLeft) {
+		Debug.Log("Pan LEFT!");
+	}
+	if (panRight) {
+		Debug.Log("Pan RIGHT!");
+	}
+
+	// Calcuate postion of camera compared to node above player
+	var cameraDelta:Vector3 = cameraNode.transform.position - transform.position;
+	var cameraLeft:Vector3 = Quaternion.AngleAxis(-25, Vector3.up) * cameraDelta;
+	var cameraRight:Vector3 = Quaternion.AngleAxis(25, Vector3.up) * cameraDelta;
+	// If user input moves camera
+	if (panLeft || panRight) {
+		// Move camera left or right
+		if (panLeft) {
+			cameraNode.transform.position = transform.position + cameraLeft;
+		}
+		if (panRight) {
+			cameraNode.transform.position = transform.position + cameraRight;
+		}
+		// Get new delta to camera
+		cameraDelta = cameraNode.transform.position - transform.position;
+	}
+	Debug.DrawRay(cameraLook.position, cameraLeft, Color.blue);
+	Debug.DrawRay(cameraLook.position, cameraRight, Color.red);
+}
+private var lockedMouse:boolean = false;
+function rotateViewByAxis() {
+	if (lockedMouse) {
+		var mouseSpeed = 3.0;
+
+		var mouseX = Input.GetAxis("Mouse X");
+		var cameraDelta = (cameraNode.transform.position - transform.position);
+		var cameraRotated:Vector3 = Quaternion.AngleAxis(mouseX * mouseSpeed, Vector3.up) * cameraDelta;
+		cameraNode.transform.position = transform.position + cameraRotated;
+
+		var mouseY = -1 * Input.GetAxis("Mouse Y");
+		cameraDelta = (cameraNode.transform.position - transform.position);
+		cameraRotated = Quaternion.AngleAxis(mouseY * mouseSpeed, cameraObject.transform.right) * cameraDelta;
+		cameraNode.transform.position = transform.position + cameraRotated;
+	
+	}
+}
+function rotateView() {
+	if (mouseLook) {
+		if (Input.GetMouseButtonDown(0)) {
+			Cursor.lockState = CursorLockMode.Locked;
+			lockedMouse = true;
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			Cursor.lockState = CursorLockMode.None;
+			lockedMouse = false;
+		}
+		rotateViewByAxis();
+	} else {
+		rotateViewByKey();
+	}
+}
+
+function clipCameraToObjects() {
+
+	var hit : RaycastHit;
+	var cameraDelta:Vector3 = cameraNode.transform.position - transform.position;
+	var cameraDirection:Vector3 = cameraDelta.normalized;
+	Debug.DrawRay(cameraLook.position, cameraDelta, Color.red);
+	var allLayersExceptPlayer:int = (1 << 9);
+	if (Physics.Raycast(cameraLook.position, cameraDelta, hit,allLayersExceptPlayer)) {
+		if(hit.distance < cameraDelta.magnitude && !hit.rigidbody || (hit.rigidbody && (hit.rigidbody.gameObject != gameObject))) {
+			Debug.Log("cliping Camera Distance");
+			cameraRaycast.transform.position = hit.point;
+		}
+	} else {
+		cameraRaycast.transform.position = cameraNode.transform.position;
+	}
+}
+function keepCameraAbovePlayer() {
+	// Get the player size, accounting for scale of the player node
+	var playerSize:double = GetComponent(SphereCollider).radius * averageVectorValue(transform.localScale);
+
+	// Camera should always be above player
+	// Future note: maybe allow camera to be below player for 2 seconds?
+	if (cameraNode.transform.position.y < cameraLook.position.y - playerSize * 0.5) {
+			Debug.Log("keeping camera up");
+		cameraNode.transform.position.y = cameraLook.position.y - playerSize * 0.5;
+	}
+
+	if (cameraNode.transform.position.y > cameraLook.position.y + (playerSize*3)) {
+			Debug.Log("maxing camera up");
+		cameraNode.transform.position.y = cameraLook.position.y + (playerSize*3);
+	}
+}
+function pushCameraBack() {
+	// Get the player size, accounting for scale of the player node
+	var playerSize:double = GetComponent(SphereCollider).radius * averageVectorValue(transform.localScale);
+
+	// Get a node above the player
+	var cameraMoveTarget:Vector3 = cameraLook.position;
+		
+	// Calcuate postion of camera compared to node above player
+	var cameraDelta:Vector3 = cameraNode.transform.position - cameraMoveTarget;
+	var cameraDirection:Vector3 = cameraDelta.normalized;
+	//cameraDirection.y += playerSize * 2;
+	//Debug.DrawRay(cameraMoveTarget, cameraDirection * maxDistance, Color.blue);
+	//Debug.DrawRay(cameraMoveTarget, cameraDirection * minDistance, Color.red);
+	if (Mathf.Abs(cameraDelta.magnitude) < minDistance) {
+			Debug.Log("pushing camera back");
+		//cameraDirection.y += playerSize;
+		cameraDirection = cameraDirection.normalized;
+		//cameraNode.transform.position = cameraMoveTarget + (cameraDirection * minDistance);
+		cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, cameraMoveTarget + (cameraDirection * minDistance), 0.05);
+		//cameraDirection.x *= -1;
+		//cameraDirection.z *= -1;
+		//cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, cameraMoveTarget + (cameraDirection * maxDistance), 0.7);
+	}
+}
+
+function limitCameraDistance() {
+	// Get the player size, accounting for scale of the player node
+	var playerSize:double = GetComponent(SphereCollider).radius * averageVectorValue(transform.localScale);
+
+	// Get a node above the player
+	var cameraMoveTarget:Vector3 = cameraLook.position;
+		
+	// Calcuate postion of camera compared to node above player
+	var cameraDelta:Vector3 = cameraNode.transform.position - cameraMoveTarget;
+	var cameraDirection:Vector3 = cameraDelta.normalized;
+	//cameraDirection.y += playerSize * 2;
+	//Debug.DrawRay(cameraMoveTarget, cameraDirection * maxDistance, Color.blue);
+	//Debug.DrawRay(cameraMoveTarget, cameraDirection * minDistance, Color.red);
+	
+/*	if (cameraNode.transform.position.y > cameraMoveTarget.y) {
+		if (Mathf.Abs(cameraDelta.x) * Mathf.Abs(cameraDelta.x) + Mathf.Abs(cameraDelta.y) * Mathf.Abs(cameraDelta.y) > minDistance * minDistance) {		
+			cameraDirection.y -= playerSize;
+			if (cameraNode.transform.position.y < cameraMoveTarget.y) {
+				cameraNode.transform.position.y = cameraMoveTarget.y + 0.1;
+			}
+			cameraDirection = cameraDirection.normalized;
+			cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, cameraMoveTarget + (cameraDirection * minDistance), 0.01);
+		}
+	}*/
+	if (Mathf.Abs(cameraDelta.magnitude) > maxDistance) {
+		Debug.Log("Pulling camera in");
+		//cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, cameraMoveTarget + (cameraDirection * maxDistance), 0.05);
+		cameraNode.transform.position = cameraMoveTarget + (cameraDirection * maxDistance);
+	}
+}
+function moveCamera() {
+	cameraObject.transform.position = Vector3.Lerp(cameraObject.transform.position, cameraRaycast.transform.position, 0.05);
+	cameraObject.transform.LookAt(cameraLook);
+}
+
+function scaleView() {
+	var wheel = Input.GetAxis("Mouse ScrollWheel");
+	if (wheel) {
+		wheel *= -10;
+		// Get the player size, accounting for scale of the player node
+		var playerSize:double = GetComponent(SphereCollider).radius * averageVectorValue(transform.localScale);
+		maxDistance = maxDistance + playerSize * wheel;
+		if (maxDistance > 25 * playerSize) {
+			maxDistance = 25 * playerSize;
+		}
+		if (maxDistance < 4 * playerSize) {
+			maxDistance = 4 * playerSize;
+		} 	
+		minDistance = maxDistance - playerSize * 3;
+	}
+}
+
+var lastPosition:Vector3 = new Vector3(0,0,0);
+function Update () {
+	scaleView();
+	rotateView();
+	pushCameraBack();
+	limitCameraDistance();
+	//keepCameraAbovePlayer();
+	orientRunnerAndLook();
+	clipCameraToObjects();
+	moveCamera();
+
+}
+
+
--- Assets/PlayerBall/PlayerBall.js
+++ Assets/PlayerBall/PlayerBall.js
@@ -1,7 +1,6 @@
 ﻿#pragma strict
 
 function Start () {
-	runnerNode = transform.FindChild("RunnerNode");
 	cages.Push(cage1);
 	cages.Push(cage2);
 	runners.Push(runner1);
@@ -16,10 +15,6 @@ function Start () {
 	startingLocation = transform.position;
 }
 
-public var cameraNode:Transform;
-public var cameraLook:Transform;
-public var cameraRaycast:Transform;
-private var runnerNode:Transform;
 private var startingLocation:Vector3 = new Vector3(0,0,0);
 function Update () {
 	if (transform.position.y < -1000) {
@@ -27,89 +22,6 @@ function Update () {
 		transform.position.y += GetComponent(SphereCollider).radius * averageVectorValue(transform.localScale);
 		gameObject.GetComponent(Rigidbody).velocity = new Vector3(0,0,0);
 	}
-}
-var lastPosition:Vector3 = new Vector3(0,0,0);
-function FixedUpdate () {
-	//runnerNode.transform.rotation.SetLookRotation(runnerNode.transform.forward * -1);
-	runnerNode.transform.eulerAngles = new Vector3(0,Camera.main.transform.eulerAngles.y,0);
-	var playerSize:double = GetComponent(SphereCollider).radius * averageVectorValue(transform.localScale);
-	/*
-	var currentPosition:Vector3 = transform.position;
-	var movement:Vector3 = lastPosition - currentPosition;
-	//if (movement.magnitude > 0.01) {
-		var direction:Vector3 = movement.anormalized;
-		Debug.DrawRay(transform.position, direction * 100, Color.white);
-		direction.x *= (5 * playerSize);
-		direction.z *= (5 * playerSize);
-		if (direction.y < -0.3) {
-			direction.y = -0.5 * playerSize;
-		} else {
-			direction.y = 0.5 * playerSize;
-		}
-		Debug.DrawRay(transform.position, direction, Color.green);
-		var newPosition:Vector3 = transform.position + direction;
-//		var newYPosition:Vector3 = new Vector3(0,newPosition.y, 0);
-//		newPosition.y = transform.position.y;
-		cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, newPosition, 0.1);
-//		cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, newYPosition, 0.08);
-		//Debug.Log(" NY: " + transform.position.y + " TY:" + newPosition.y + " CY:" + cameraNode.transform.position.y);
-		//Debug.Log(" Adding : " + transform.position.y + " TY:" + newPosition.y + " CY:" + cameraNode.transform.position.y);
-		//var delta:Vector3 = new Vector3((newPosition.x - cameraNode.transform.position.x) * 0.1,  (newPosition.y - cameraNode.transform.position.y) * 0.1, (newPosition.z - cameraNode.transform.position.z) * 0.1);
-		//Debug.Log(" Delta: " + delta);
-		//Debug.Log("Camera Position Before:" + cameraNode.transform.position);
-		//cameraNode.transform.position += delta;
-		//Debug.Log("Camera Position After:" + cameraNode.transform.position);
-		lastPosition = currentPosition;
-	//}
-	*/
-	cameraLook.position = transform.position;
-	cameraLook.position.y += playerSize * 1;
-	var cameraMoveTarget:Vector3 = transform.position;
-	cameraMoveTarget.y += playerSize;
-	//cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, new Vector3(cameraNode.transform.position.x, cameraLook.position.y, cameraNode.transform.position.z), 0.5);
-	var cameraDelta:Vector3 = cameraNode.transform.position - cameraMoveTarget;
-	var cameraDirection:Vector3 = cameraDelta.normalized;
-	var maxDistance:double = playerSize * 6;
-	var minDistance:double = playerSize * 4;
-	//cameraDirection.y += playerSize * 2;
-	//Debug.DrawRay(cameraMoveTarget, cameraDirection * maxDistance, Color.blue);
-	//Debug.DrawRay(cameraMoveTarget, cameraDirection * minDistance, Color.red);
-	if (Mathf.Abs(cameraDelta.magnitude) < minDistance) {
-		cameraDirection.y += playerSize;
-		cameraDirection = cameraDirection.normalized;
-		//cameraNode.transform.position = cameraMoveTarget + (cameraDirection * minDistance);
-		cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, cameraMoveTarget + (cameraDirection * minDistance), 0.05);
-		//cameraDirection.x *= -1;
-		//cameraDirection.z *= -1;
-		//cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, cameraMoveTarget + (cameraDirection * maxDistance), 0.7);
-	} else {
-		if (cameraNode.transform.position.y > cameraMoveTarget.y) {
-			if (Mathf.Abs(cameraDelta.x) * Mathf.Abs(cameraDelta.x) + Mathf.Abs(cameraDelta.y) * Mathf.Abs(cameraDelta.y) > minDistance * minDistance) {		
-				cameraDirection.y -= playerSize;
-				cameraDirection = cameraDirection.normalized;
-				cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, cameraMoveTarget + (cameraDirection * minDistance), 0.01);
-			}
-		}
-		if (Mathf.Abs(cameraDelta.magnitude) > maxDistance) {
-			cameraNode.transform.position = Vector3.Lerp(cameraNode.transform.position, cameraMoveTarget + (cameraDirection * maxDistance), 0.05);
-			//cameraNode.transform.position = cameraMoveTarget + (cameraDirection * maxDistance);
-		}
-	}
-	
-	var hit : RaycastHit;
-	cameraDelta = cameraNode.transform.position - cameraLook.position;
-	cameraDirection = cameraDelta.normalized;
-	Debug.DrawRay(cameraLook.position, cameraDelta, Color.white);
-	if (Physics.Raycast(cameraLook.position, cameraDirection, hit, 1 << 8)) {
-		if(hit.distance < cameraDelta.magnitude && !hit.rigidbody || (hit.rigidbody && (hit.rigidbody.gameObject != gameObject))) {
-			cameraRaycast.transform.position = hit.point;
-		}
-		//var distanceToGround = hit.distance;
-	} else {
-		cameraRaycast.transform.position = cameraNode.transform.position;
-	}
-		
-
 }
 
 var childCount = 0;
@@ -138,20 +50,19 @@ function OnCollisionEnter(collisionInfo:Collision)
 			   	var radius = playerSize.radius * averageVectorValue(transform.localScale);
 			   	var pickupRadius = pickupAble.GetComponent(SphereCollider).radius * averageVectorValue(worldObject.transform.localScale);
 			   	if (pickupRadius * 3 < radius  && pickupRadius * 20) {
-			   		Debug.Log("Pickup Radius = " + pickupRadius + " Player Radius = " + radius);
 				    var deltaVec:Vector3 = pickupAble.position - transform.position;
 				    pickupAble.name += "." + childCount;
 				    worldObject.name += "." + childCount;
 				    childCount++;
-				    worldObject.transform.SetParent(transform);
-				    pickupAbleCollider.material = GetComponent(SphereCollider).material;
-				    pickupAbleCollider.enabled = true;
-				    pickupAble.gameObject.layer = gameObject.layer;
 				    var worldObjectColliders:Array = worldObject.GetComponents(Collider);
 				    for( var i = 0; i < worldObjectColliders.length; ++i ){
 				    	(worldObjectColliders[i] as Collider).enabled = false;
 				    }
+				    pickupAbleCollider.material = GetComponent(SphereCollider).material;
+				    pickupAbleCollider.enabled = true;
+				    pickupAble.gameObject.layer = gameObject.layer;
 				    Destroy (worldObject.GetComponent.<Rigidbody>());
+				    worldObject.transform.SetParent(transform);
 				   	worldObject.transform.localPosition = worldObject.transform.localPosition.normalized * playerSize.radius;
 				   	worldObject.transform.localPosition -= pickupAble.transform.localPosition;
 				   	// r3 = r1 (1 + 1/3r2^3/ r1^3)
@@ -206,7 +117,7 @@ function UpdateVisibleRunner () {
 	for( i = 0; i < runners.length; ++i) {
 		(runners[i] as Transform).gameObject.SetActive(false);
 	}
-	(runners[currentRunner] as Transform).gameObject.SetActive(true);
+	//(runners[currentRunner] as Transform).gameObject.SetActive(true);
 }
 
 function UpdateVisibleCages () {
